0c4ad15b22602d75f3d425d06f0308373fd1e324
shreyask1416 <shreya.sk@robosoftin.com>
2021-02-26 14:06:14 +0530

    projectss

--- src/components/FilmDetails/FilmDetails.tsx
+++ src/components/FilmDetails/FilmDetails.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import styles from './FilmDetails.module.css';
 import { useSpeciesContext} from "../../SpeciesContext";
 const FilmDetails = () => {
     const {title,epId,director,producer,releaseDate,opening}=useSpeciesContext();
+    const [img,setImg]=useState<any>();
+    const getImage=()=>{
+      if(sessionStorage.getItem(`${title}imgs`)){
+         setImg(sessionStorage.getItem(`${title}imgs`)||'{}')
+      }
+      else{
+      const url="https://picsum.photos/200/300/?random&cb=" + (+new Date()) + "";
+      setImg(url);
+      sessionStorage.setItem(`${title}imgs`,url);
+      }
+    }
+    React.useEffect(() => {
+      getImage();
+    }, [title]);
 return (
 <div className={styles.FilmDetails }>
 <div className={styles.box} >
               <div className={styles.imgs}>
-              <img src={"https://picsum.photos/200/300/?random&cb=" + (+new Date()) + ""}></img></div>
+              <img src={img}></img></div>
               <div className={styles.info}>
                 <h1>{title}</h1>
                 <h2 className={styles.height}>Episode Id : <span className={styles.hei}>{epId}</span></h2>
--- src/components/Films/Films.tsx
+++ src/components/Films/Films.tsx
@@ -8,10 +8,15 @@ const Films = () => {
   const [prev, setPrev] =React.useState(false);
   const [next, setNext] =React.useState(false);
   const [loading, setLoading] = React.useState(true);
-  const {setTitle,setEpId,setOpening,setDirector,setProducer,setReleaseDate }=useSpeciesContext();
+  const {setTitle,setEpId,setOpening,setDirector,setProducer,setReleaseDate,title }=useSpeciesContext();
   let history=useHistory();
     const getCharcter = async () => {
-      setLoading(true);
+    
+      if(sessionStorage.getItem('films')){
+        setDetails(JSON.parse(sessionStorage.getItem('films')||'{}'))
+        
+      }
+      else{
       await fetch(`https://swapi.dev/api/films/`)
         .then((response) => response.json())
         .then((responseData) => {
@@ -24,60 +29,23 @@ const Films = () => {
             setNext(true);
             }
           setDetails(responseData.results);
-          setLoading(false);
+    
+          sessionStorage.setItem('films',JSON.stringify(responseData.results));
         })
         .catch((error) => {
           console.log(error);
         });
     };
-  
-  
-    const nextCharcater = async (charcter:any) => {
-      setLoading(true);
-      await fetch(charcter.next)
-        .then((response) => response.json())
-        .then((responseData) => {
-          setCharacter(responseData);
-          if(responseData.next === null) {
-            setNext(true);
-            }
-            if(responseData.prev !== null) {
-            setPrev(false);
-            }
-          setDetails(responseData.results);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-    const previousCharcater = async (charcter:any) => {
-      setLoading(true);
-      await fetch(charcter.previous)
-        .then((response) => response.json())
-        .then((responseData) => {
-          setCharacter(responseData);
-          if(responseData.previous === null) {
-            setPrev(true);
-            }
-            if(responseData.next !== null) {
-            setNext(false);
-            }
-          setDetails(responseData.results);
-          setLoading(false);
-                  })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
+  }
+    
   React.useEffect(() => {
       getCharcter();
     }, []);
     return (
         <div className={styles.Films}>
-            {loading && <div className={styles.loading}>
+            {/* {loading && <div className={styles.loading}>
 <h1>Loading ... </h1>
-</div>}
+</div>} */}
 
              <div>
               {Details.map((item:any)=>{
